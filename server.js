093e646a431ccd0bd75e3e487e4d5dcaf202e9a1
Ankit <147058214+Ankit9134@users.noreply.github.com>
2026-04-13 15:32:50 +0530

    Implement database connection in server.js
    Add database connection logic to server initialization.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const app = require('./src/app');
 const dotenv = require('dotenv');
-
+const db = require('./src/config/database')
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
+db.getConnection()
+    .then(connection => {
+        console.log('Database connected successfully');
+        connection.release();
+    })
+    .catch(err => {
+        console.error(' Database connection failed:', err.message);
+    });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.DB_HOST}`);
     console.log(`Server is running on port ${PORT}`);
