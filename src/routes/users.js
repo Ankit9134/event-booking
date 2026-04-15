b4f144fb13c65d487dc31a4444d253fc68213c81
Ankit <147058214+Ankit9134@users.noreply.github.com>
2026-04-15 19:38:04 +0530

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,8 +6,6 @@ const { validateUser, validateUserUpdate } = require('../middleware/validate');
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', validateUser, userController.createUser);
-// router.put('/:id', validateUserUpdate, userController.updateUser);
-router.delete('/:id', userController.deleteUser);
 router.get('/:id/bookings', userController.getUserBookings);
 
 module.exports = router;
